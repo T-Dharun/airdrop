de9f74b1ec40cd73e5b7ad76b61623781f4e43cd
T-Dharun <dharuneee007@gmail.com>
2025-09-05 19:13:12 +0530

    fix: Remove http server listening

--- server/server.js
+++ server/server.js
@@ -5,10 +5,4 @@ const WebSocketServer = require("./ws/WebSocketServer");
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => res.send("server alive."));
-
-const PORT = 5001;
-app.listen(PORT, () => console.log(`HTTP server running on ${PORT}`));
-
-
 new WebSocketServer(5000);
